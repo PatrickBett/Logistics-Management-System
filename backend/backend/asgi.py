701541aecfaf32d9f6f80d5e1f2667e4052700c3
PatrickBett <patrickbett018@gmail.com>
2026-03-10 19:27:43 +0300

    asgi update

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,7 +1,13 @@
+import os
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()  # ensures Django apps are ready
 from .middleware import JWTAuthMiddlewareStack
 from logistics.routing import websocket_urlpatterns
 from channels.routing import URLRouter
-from channels.routing import ProtocolTypeRouter
+
 from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
