53acac3875f26cc5d8473123d372cffcc537be04
PatrickBett <patrickbett018@gmail.com>
2026-03-12 14:31:18 +0300

    update channel layers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,7 +68,9 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [
+                "rediss://default:PASSWORD@charmed-gar-66500.upstash.io:6379"
+            ],
         },
     },
 }
