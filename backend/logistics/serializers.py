38bd8727bd96cb621a0bebec89e4bc558626003e
PatrickBett <patrickbett018@gmail.com>
2026-05-09 10:40:47 +0300

    license number

--- backend/logistics/serializers.py
+++ backend/logistics/serializers.py
@@ -57,7 +57,7 @@ class DriverRegisterSerializer(serializers.ModelSerializer):
         try:
             driver = Driver.objects.get(license_no=value)
         except Driver.DoesNotExist:
-            raise serializers.ValidationError("License number NOT Registered by Admin")
+            raise serializers.ValidationError("Driver with this License number NOT Registered by Admin")
 
         if driver.user:
             raise serializers.ValidationError(
